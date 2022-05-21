5c823f795bc6fb6b91cce38ab7849074d4c0b172
Mustafa <mstfdakhil@gmail.com>
2022-05-21 14:44:30 +0300

    Replace lazy column with scrollable column

--- app/src/main/java/com/martin/meanbean/ui/bean_details/BeanPage.kt
+++ app/src/main/java/com/martin/meanbean/ui/bean_details/BeanPage.kt
@@ -1,19 +1,20 @@
 package com.martin.meanbean.ui.bean_details
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.rememberImagePainter
 import com.martin.meanbean.domain.entities.BeanEntity
@@ -22,6 +23,7 @@ import com.martin.meanbean.ui.common.Loading
 
 @Composable
 fun BeanPage(beanId: Int?, viewModel: BeanPageViewModel = hiltViewModel()) {
+
 	LaunchedEffect(key1 = beanId, block = {
 		viewModel.fetch(beanId)
 	})
@@ -36,22 +38,50 @@ fun BeanPage(beanId: Int?, viewModel: BeanPageViewModel = hiltViewModel()) {
 
 @Composable
 fun BeanDetails(bean: BeanEntity) {
-	Column {
-		Image(
-			painter = rememberImagePainter(bean.image),
-			contentDescription = bean.name,
-			contentScale = ContentScale.FillWidth,
-			alpha = 0.8f,
-			modifier = Modifier
-					.fillMaxWidth()
-					.height(450.dp)
-		)
-		Text(
-			text = bean.name,
-			textAlign = TextAlign.Center,
-			modifier = Modifier
-					.fillMaxWidth()
-					.align(Alignment.CenterHorizontally)
-		)
+	Column(
+		Modifier
+				.fillMaxSize()
+				.verticalScroll(rememberScrollState())
+	) {
+		BeanImage(image = bean.image, description = bean.name)
+		BeanTitle(bean.name)
+		BeanDescription(bean.description)
 	}
+}
+
+@Composable
+fun BeanImage(image: String, description: String) {
+	Image(
+		painter = rememberImagePainter(image),
+		contentDescription = description,
+		contentScale = ContentScale.FillWidth,
+		alpha = 0.8f,
+		modifier = Modifier
+				.fillMaxWidth()
+				.height(450.dp)
+	)
+}
+
+@Composable
+fun BeanTitle(name: String) {
+	Text(
+		text = name,
+		textAlign = TextAlign.Center,
+		modifier = Modifier
+				.fillMaxWidth()
+				.padding(horizontal = 24.dp, vertical = 12.dp),
+		fontWeight = FontWeight.Bold,
+		fontSize = 18.sp
+	)
+}
+
+@Composable
+fun BeanDescription(description: String) {
+	Text(
+		text = description,
+		textAlign = TextAlign.Start,
+		modifier = Modifier
+				.fillMaxWidth()
+				.padding(horizontal = 24.dp, vertical = 12.dp)
+	)
 }
